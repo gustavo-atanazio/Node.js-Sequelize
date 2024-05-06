1861f9ac69c851b0aaaba1c909352b67a2e08afb
Gustavo <gus.atanazio@gmail.com>
2024-05-06 17:21:53 -0300

    Definindo alias path para a pasta src

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const app = require('./src/app.js');
+const path = require('path');
+const moduleAlias = require('module-alias');
+
+// Definir o caminho absoluto para a pasta 'src'
+const srcPath = path.join(__dirname, 'src');
+
+// Adicionar alias para os módulos na pasta 'src'
+moduleAlias.addAlias('@app', srcPath);
+
+const app = require('@app/app.js');
 
 const PORT = 3000;
 
-app.listen(PORT, () => console.log(`Servidor rodando em localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
