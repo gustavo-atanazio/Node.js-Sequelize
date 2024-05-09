7cb7860d1091953fb599960793ae236d701d98d1
Gustavo <gus.atanazio@gmail.com>
2024-05-09 17:44:15 -0300

    Adicionando rotas faltantes

--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -6,6 +6,9 @@ const router = Router();
 const pessoaController = new PessoaController();
 
 router.get('/pessoas', (req, res) => pessoaController.getAll(req, res));
+router.get('/pessoas/:id', (req, res) => pessoaController.getOneById(req, res));
+router.post('/pessoas', (req, res) => pessoaController.createNew(req, res));
 router.put('/pessoas/:id', (req, res) => pessoaController.update(req, res));
+router.delete('/pessoas/:id', (req, res) => pessoaController.delete(req, res));
 
 module.exports = router;
